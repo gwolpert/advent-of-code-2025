466442b8fe350572d2ea0cf8d9be87ad82faa6ae
G Wolpert <gwolpert@users.noreply.github.com>
2025-12-01 14:53:42 +0100

    Day 1

--- src/days/01.test.ts
+++ src/days/01.test.ts
@@ -1,18 +1,18 @@
 import { executePart, importDay } from "../runner.ts";
 import { describe, test, expect } from "bun:test";
 describe(`Day ${1}`, async () => {
-	const input = "";
+	const input = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82";
 	const day = await importDay(1);
 
 	test("Part 1", async () => {
 		const part = day[1];
 		const result = await executePart("Part 1", part, input);
-		expect(result).toBe(0);
+		expect(result).toBe(3);
 	});
 
 	test("Part 2", async () => {
 		const part = day[2];
 		const result = await executePart("Part 2", part, input);
-		expect(result).toBe(0);
+		expect(result).toBe(6);
 	});
 });
--- src/days/01.ts
+++ src/days/01.ts
@@ -1,10 +1,27 @@
 import type { Day } from "../runner.ts";
 
+type Direction = "L" | "R";
+type Rotation = { curr: number; next: number; dir: Direction; dist: number };
+const dirs: Record<Direction, number> = { L: -1, R: 1 } as const;
+
+const solve = (input: string, countFn: (r: Rotation) => number) =>
+  input
+    .matchAll(/^([LR])(\d+)$/gm)
+    .map((x) => ({ dir: x[1] as Direction, dist: +x[2]! }))
+    .reduce(
+      ({ curr, count }, { dir, dist }) => {
+        const next = (curr + dirs[dir] * dist) % 100;
+        count += countFn({ curr, next, dir, dist });
+        return { curr: next, count };
+      },
+      { curr: 50, count: 0 }
+    ).count;
+
 export default {
-	1: (input: string) => {
-		return 0;
-	},
-	2: (input: string) => {
-		return 0;
-	},
+  1: (input) => solve(input, ({ next }) => +!next),
+  2: (input) =>
+    solve(input, ({ curr, dir, dist }) => {
+      const delta = { L: 0, R: 100 }[dir] - curr * dirs[dir];
+      return Math.max(0, ~~((dist - delta + 99) / 100));
+    }),
 } satisfies Day;
