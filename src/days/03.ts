962667732b77219a9558f29ce9bd5bda64c36800
G Wolpert <gwolpert@users.noreply.github.com>
2025-12-03 12:07:26 +0100

    Day 3

--- src/days/03.ts
+++ src/days/03.ts
@@ -1,26 +1,25 @@
 import type { Day } from "../runner.ts";
 
-const solve = (mapBank: (bank: number[]) => number) => (input: string) => {
-  const banks = input.split("\n").map((bank) => Array.from(bank).map(Number));
-  return banks.map(mapBank).reduce((total, joltage) => total + joltage, 0);
+const solve = (enabled: number) => (input: string) => {
+  const findBest = (bank: number[], needed: number): number[] => {
+    if (needed === 0) return [];
+    for (let digit = 9; digit >= 0; digit--) {
+      const i = bank.findIndex((battery, position) => {
+        return battery === digit && bank.length - position >= needed;
+      });
+      if (~i) return [digit, ...findBest(bank.slice(i + 1), needed - 1)];
+    }
+    return [];
+  };
+
+  const banks = input.split("\n").map((bank) => {
+    const battery = Array.from(bank).map(Number);
+    return Number(findBest(battery, enabled).join(""));
+  });
+  return banks.reduce((total, joltage) => total + joltage, 0);
 };
 
 export default {
-  1: solve((bank) => {
-    const joltages = bank.flatMap((first, i) =>
-      Array.from(bank.slice(i + 1)).map((second) => first * 10 + second)
-    );
-    return Math.max(...joltages);
-  }),
-  2: solve((batteries) => {
-    const result = batteries.reduce(
-      (s, battery) => {
-        while (s.skip > 0 && (s.selected.at(-1) ?? Infinity) < battery)
-          (s.selected.pop(), s.skip--);
-        return (s.selected.push(battery), s);
-      },
-      { selected: new Array<number>(), skip: batteries.length - 12 }
-    );
-    return +result.selected.slice(0, 12).join("");
-  }),
+  1: solve(2),
+  2: solve(12),
 } satisfies Day;
