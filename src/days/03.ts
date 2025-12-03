8deeb8634a5b95bff3b75baf2ec5f13c2de22a61
G Wolpert <gwolpert@users.noreply.github.com>
2025-12-03 12:48:20 +0100

    Day 3

--- src/days/03.ts
+++ src/days/03.ts
@@ -1,18 +1,18 @@
 import type { Day } from "../runner.ts";
 
-const solve = (enabled: number) => (input: string) => {
-  const findBest = (bank: number[], needed: number): number[] => {
-    if (needed === 0) return [];
-    if (needed === bank.length) return bank;
-    for (let digit = 9; digit >= 0; digit--) {
-      const findIndex = (battery: number, pos: number) =>
-        battery === digit && bank.length - pos >= needed;
-      const index = bank.findIndex(findIndex) + 1;
-      if (index) return [digit, ...findBest(bank.slice(index), --needed)];
-    }
-    return [];
-  };
+const findBest = (bank: number[], needed: number): number[] => {
+  if (needed === 0) return [];
+  if (needed === bank.length) return bank;
+  for (let digit = 9; digit >= 0; digit--) {
+    const findIndex = (battery: number, pos: number) =>
+      battery === digit && bank.length - pos >= needed;
+    const index = bank.findIndex(findIndex) + 1;
+    if (index) return [digit, ...findBest(bank.slice(index), --needed)];
+  }
+  return [];
+};
 
+const solve = (enabled: number) => (input: string) => {
   const banks = input.split("\n").map((line) => {
     const bank = Array.from(line).map(Number);
     return Number(findBest(bank, enabled).join(""));
